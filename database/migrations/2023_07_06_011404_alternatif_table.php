bf35d58f30c771e6626076fb35a3b8ebc2bf447c
Haidar Azmi Rohman <95571529+haazman@users.noreply.github.com>
2023-07-10 20:32:01 +0700

    migration

---
+++ database/migrations/2023_07_06_011404_alternatif_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('alternatif', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->double('stamina');
+            $table->double('posture');
+            $table->double('finishing');
+            $table->double('dribbling');
+            $table->double('header');
+            $table->double('attitude');
+            $table->double('indeks_vikor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
